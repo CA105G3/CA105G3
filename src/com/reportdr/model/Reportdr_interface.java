33bf9416f2a5fb833a697497ebeef6e6aa65cc08
alcawei <alcawei@gmail.com>
2018-12-11 22:07:13 +0800

    :俊維第一次上傳:

--- src/com/reportdr/model/Reportdr_interface.java
+++ src/com/reportdr/model/Reportdr_interface.java
@@ -3,7 +3,7 @@ package com.reportdr.model;
 import java.util.*;
 
 public interface Reportdr_interface {
-
+ 
 	public void insert(ReportdrVO reportdrVO);
 	public void update(ReportdrVO reportdrVO);
 	public void delete(ReportdrVO reportdrVO);
