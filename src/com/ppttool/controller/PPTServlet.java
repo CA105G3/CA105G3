78c20a25b26ac7c0bcb6492e5820cb0bcce9cf2d
Jimlee0507 <atos0732@gmail.com>
2018-12-16 22:14:05 +0800

    add PPTToolService,PPTServlet(Controller),PPToolDAO,select_page_ppt,12/16

---
+++ src/com/ppttool/controller/PPTServlet.java
@@ -0,0 +1,96 @@
+package com.ppttool.controller;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ppttool.model.PPTToolService;
+import com.ppttool.model.PPTToolVO;
+
+/**
+ * Servlet implementation class PPTServlet
+ */
+@WebServlet("/PPTServlet")
+public class PPTServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req,res);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String str = req.getParameter("pptno");
+				if (str == null || (str.trim()).length() == 0) {
+					errorMsgs.add("請輸入PPT編號");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String pptno = null;
+				try {
+					pptno = new String(str);
+				} catch (Exception e) {
+					errorMsgs.add("PPT編號格式不正確");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************2.開始查詢資料*****************************************/
+				PPTToolService pptSvc = new PPTToolService();
+				PPTToolVO pptVO = pptSvc.getOnePPT(pptno);
+				if (pptVO == null) {
+					errorMsgs.add("查無資料");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("empVO", pptVO); // 資料庫取出的empVO物件,存入req
+				String url = "/emp/listOneEmp.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/emp/select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+	}
+}
