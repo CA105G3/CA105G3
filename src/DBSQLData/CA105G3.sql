efcbae6b2b74b74d71e8aaf00dd0e2b446be4ce6
Steven <noir6647@gmail.com>
2018-12-25 16:14:58 +0800

    update sql command : add & update column for memberchef, menu

--- src/DBSQLData/CA105G3.sql
+++ src/DBSQLData/CA105G3.sql
@@ -258,17 +258,19 @@ INSERT INTO TRACE VALUES ('M0001','ACT0001');
 -- create memberchef
 -----------------------------------------------
 CREATE TABLE memberchef(
-    chefno      VARCHAR2(8 BYTE) NOT NULL ,
-    memno       VARCHAR2(5 BYTE) NOT NULL ,
-    chefpic     BLOB ,
-    chefdescrip VARCHAR2(120 BYTE) ,
-    chefstatus  VARCHAR2(9 BYTE) NOT NULL ,
-    chephone    NUMBER ,
-    cheaddr     VARCHAR2(60 BYTE) ,
-    cherep      VARCHAR2(15 BYTE) ,
+    chefno          VARCHAR2(8 BYTE) NOT NULL ,
+    memno           VARCHAR2(5 BYTE) NOT NULL ,
+    chefName        VARCHAR2(30 BYTE) NOT NULL ,
+    chefStoreName   VARCHAR2(30 BYTE) NOT NULL ,
+    chefpic         BLOB ,
+    chefdescrip     VARCHAR2(300 BYTE) ,
+    chefstatus      VARCHAR2(9 BYTE) NOT NULL ,
+    chefphone       NUMBER ,
+    chefaddr        VARCHAR2(60 BYTE) ,
+    chefrep         VARCHAR2(15 BYTE) ,
     CONSTRAINT memberchef_pk PRIMARY KEY (chefno),
     CONSTRAINT memberchef_fk FOREIGN KEY (memno) REFERENCES MEMBER (memno),
-    CONSTRAINT CHK_chefenable CHECK (chefstatus in('停用','啓用','審核中')));
+    CONSTRAINT CHK_chefstatus CHECK (chefstatus in('停用','啓用','審核中')));
     
 CREATE SEQUENCE memberchef_seq
 INCREMENT BY 1
@@ -277,7 +279,7 @@ NOMAXVALUE
 NOCYCLE
 NOCACHE;
 
-INSERT INTO memberchef VALUES ('CHEF'||lpad(to_char(memberchef_seq.NEXTVAL), 4, '0'),'M0001',NULL,'自我介紹','啓用','03987456','中央大學內','三普');
+INSERT INTO memberchef VALUES ('CHEF'||lpad(to_char(memberchef_seq.NEXTVAL), 4, '0'),'M0001','孫小美','愛的廚房',NULL,'自我介紹','啓用','03987456','中央大學內','三普');
 
 -----------------------------------------------
 -- create menu
@@ -288,10 +290,10 @@ CREATE TABLE menu(
     unitprice   NUMBER ,
     maincourse  VARCHAR2(24 BYTE) NOT NULL ,
     menupic     BLOB ,
-    delivertime VARCHAR2(12 BYTE) ,
+    deliverable VARCHAR2(12 BYTE) ,
     CONSTRAINT menu_pk PRIMARY KEY (menuno),
     CONSTRAINT menu_fk FOREIGN KEY (chefno) REFERENCES memberchef (chefno),
-    CONSTRAINT CHK_delivertime CHECK (delivertime in('可送餐','不可送餐'))
+    CONSTRAINT CHK_deliverable CHECK (deliverable in('可送餐','不可送餐'))
 );
     
 CREATE SEQUENCE menu_seq
