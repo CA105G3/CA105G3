d853249192e3d95fce04109a923e87f90a8ed949
Steven <noir6647@gmail.com>
2019-01-22 23:39:43 +0800

    fix connection pool

--- src/DBSQLData/Menu假資料.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO menu VALUES (to_char(current_date, 'YYYYMMDD')||'-'||lpad(to_char(menu_seq.NEXTVAL), 4, '0'),'CHEF0001','150','佛跳牆',NULL,'不可送餐');
-INSERT INTO menu VALUES (to_char(current_date, 'YYYYMMDD')||'-'||lpad(to_char(menu_seq.NEXTVAL), 4, '0'),'CHEF0001','80','脆皮燒鴨飯',NULL,'可送餐');
-COMMIT;
